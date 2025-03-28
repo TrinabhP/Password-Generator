9247d2c0943325120caa4928e57be0e8d32372bb
Trinabh <trinabhp@gmail.com>
2025-03-28 10:05:22 -0700

    Updated Project

--- generator.py
+++ generator.py
@@ -12,6 +12,12 @@ def main():
 	change_password = input("\nDo you want to change your password?(y/n) ")
 	if (change_password == 'y'):
 		length = int(input("\nHow long do you want your password to be?\nEnter a number: "))
+
+		# makes sure password length is greater than 13
+		while (length < 14):
+			print("\nA strong passcode requires more than 13 characters!")
+			length = int(input("\nHow long do you want your password to be?\nEnter a number: "))
+
 		symbols = input("\nDo you want symbols in your password?(y/n) ")
 		if (symbols == 'y' or symbols == 'Y'):
 			symbols = True
@@ -35,9 +41,12 @@ def main():
 
 		password = generate(length, symbols, uppercase, digits)
 
-	print("\n-----------------------------------\n")
+		print("\n----------------------------------------\n")
 
-	print("\nYour new password is: " + password + "\nMake sure to save it!\n")
+		print("\nYour new password is: " + password + "\nMake sure to save it!\n")
+
+	else:
+		print("\n----------------------------------------\n")
 
 
 if __name__ == '__main__':
