006508959f67181a67041894134582855542b8a5
Trinabh <trinabhp@gmail.com>
2025-04-02 20:28:44 -0700

    Updated Project

--- GUI.py
+++ GUI.py
@@ -23,19 +23,19 @@ def generate(symbols=True, uppercase=True, digits=True):
 	output_string.set("".join(password))
 
 # window
-window = ttk.Window(themename = 'sandstone')
+window = ttk.Window(themename = 'darkly')
 window.title("Password-Generator")
 window.geometry('300x150')
 
 # title
-title_label = ttk.Label(master = window, text = 'Password Generator', font= 'Calibri 24 bold')
+title_label = ttk.Label(master = window, text = 'Password Generator', font= ('Segoe UI', 24, 'bold'), style='info.TLabel')
 title_label.pack()
 
 # input field
 input_frame = ttk.Frame(master = window)
 entry_int = tk.IntVar()
 entry = ttk.Entry(master = input_frame, textvariable = entry_int)
-button = ttk.Button(master = input_frame, text = 'Generate', command = generate)
+button = ttk.Button(master = input_frame, text = 'Generate', command = generate, style='info.TButton')
 entry.pack(side = 'left', padx = 10)
 button.pack(side = 'left')
 input_frame.pack(pady = 10)
@@ -45,7 +45,7 @@ output_string = tk.StringVar()
 output_label = ttk.Label(
 	master = window, 
 	text = "Password", 
-	font = 'Calibri 24', 
+	font = ('Segoe UI', 24), 
 	textvariable = output_string)
 output_label.pack(pady = 5)
 
